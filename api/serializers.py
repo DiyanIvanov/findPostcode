a4c53469e14e0e96a51631015aa8db098f3a2b0f
Diyan Ivanov <coldys_@hotmail.com>
2026-03-20 21:32:47 +0000

    feat: add serializer for batch input data

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from api.models import Postcode
 
 
@@ -9,3 +8,21 @@ class PostcodeSerializer(serializers.ModelSerializer):
         fields = ('postcode', 'district', 'area', 'eastings', 'northings', 'latitude', 'longitude')
 
 
+class BatchSerializer(serializers.Serializer):
+    postcodes = serializers.ListField(
+        child=serializers.CharField(),
+        allow_empty=False
+    )
+
+    def validate_postcodes(self, value):
+        cleaned = []
+
+        for postcode in value:
+            postcode = postcode.strip().upper()
+
+            if not postcode:
+                raise serializers.ValidationError("Empty postcode not allowed")
+
+            cleaned.append(postcode)
+
+        return cleaned
