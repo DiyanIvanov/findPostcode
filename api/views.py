e716e51517431b478102b12a376a0154ea504ec8
Diyan Ivanov <coldys_@hotmail.com>
2026-03-20 21:33:21 +0000

    feat: add batch endpoint view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, views, status
 from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
 from api.models import Postcode
-from api.serializers import PostcodeSerializer
+from api.serializers import PostcodeSerializer, BatchSerializer
 
 # Create your views here.
 class PostcodeViewSet(generics.ListAPIView):
@@ -22,3 +23,29 @@ class PostcodeView(generics.RetrieveAPIView):
             return Postcode.objects.get(postcode=postcode)
         except Postcode.DoesNotExist:
             raise NotFound(f'Postcode {postcode} not found')
+
+
+class PostcodeBatchView(views.APIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = BatchSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        postcodes = serializer.validated_data['postcodes']
+
+        results = []
+
+        for postcode in postcodes:
+            # replace with your actual lookup logic
+            obj = Postcode.objects.filter(postcode=postcode).first()
+
+            if obj:
+                results.append(PostcodeSerializer(obj).data)
+            else:
+                results.append({
+                    "postcode": postcode,
+                    "error": "Not found"
+                })
+
+        return Response(results, status=status.HTTP_200_OK)
+
