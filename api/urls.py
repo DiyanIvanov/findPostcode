3a7ae9979e9f10b3901f59caafa1b20fb9000b27
Diyan Ivanov <coldys_@hotmail.com>
2026-03-20 20:58:34 +0000

    feat: add batch endpoint url to api urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
-
-from api.views import PostcodeView
+from django.urls import path
+from api.views import PostcodeView, PostcodeBatchView
 
 urlpatterns = [
     path('postcode/<str:postcode>/', PostcodeView.as_view(), name='get_postcode'),
+    path('batch/', PostcodeBatchView.as_view(), name='batch_endpoint'),
 ]
