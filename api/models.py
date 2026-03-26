71dc25b6f6291ce676c466d2347d3315d66f10a6
Diyan Ivanov <coldys_@hotmail.com>
2026-03-26 20:49:09 +0000

    refactor: use appropriate data types in Postcode model

--- api/models.py
+++ api/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Postcode(models.Model):
     postcode = models.CharField(max_length=10, unique=True)
-    area = models.CharField(max_length=10)
-    district = models.CharField(max_length=10)
-    eastings = models.CharField(max_length=10)
-    northings = models.CharField(max_length=10)
-    latitude = models.CharField(max_length=10)
-    longitude = models.CharField(max_length=10)
+    area = models.CharField(max_length=2)
+    district = models.CharField(max_length=5)
+    eastings = models.IntegerField()
+    northings = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
 
     def __str__(self):
