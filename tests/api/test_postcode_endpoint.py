d274b5df7820759b25c5a207d788007d9d7f6229
Diyan Ivanov <coldys_@hotmail.com>
2026-03-29 16:45:03 +0100

    test: add DRF BasicAuthentication to postcode test suite

--- tests/api/test_postcode_endpoint.py
+++ tests/api/test_postcode_endpoint.py
@@ -2,6 +2,8 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from api.models import Postcode
+from accounts.models import FindPostcodeUser
+from rest_framework.authtoken.models import Token
 
 
 class PostcodeViewTest(APITestCase):
@@ -19,16 +21,25 @@ class PostcodeViewTest(APITestCase):
             latitude = -1.9156917,
             longitude = 52.4759231
         )
+        self.user = FindPostcodeUser.objects.create_user(
+            username='test_user',
+            password='testp@ssword123',
+        )
+        user = FindPostcodeUser.objects.get(username='test_user')
+        self.token, _ = Token.objects.get_or_create(user=user)
 
     def test_missing_postcode_test_returns_400(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
         response = self.client.get(self.missing_postcode_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_valid_postcode_returns_200(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
         response = self.client.get(self.valid_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_invalid_postcode_returns_404(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
         response = self.client.get(self.invalid_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
