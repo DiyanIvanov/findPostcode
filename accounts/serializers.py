45e8dddad3383cebde4856c96734a06b9fd67a30
Diyan Ivanov <coldys_@hotmail.com>
2026-04-10 19:14:59 +0100

    feat: add password validation to user registration serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 UserModel = get_user_model()
 
@@ -12,6 +14,13 @@ class FindPostcodeUserSerializer(serializers.ModelSerializer):
                 'write_only': True,
             }
         }
+
+    def validate_password(self, password):
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return password
 
     def create(self, validated_data):
         return UserModel.objects.create_user(**validated_data)
