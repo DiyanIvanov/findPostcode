0c358f123ec62fccb92d1aa3cd372de766e74b95
Diyan Ivanov <coldys_@hotmail.com>
2026-04-11 18:39:25 +0100

    feat: add db_index to postcode column for faster lookups

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Postcode(models.Model):
-    postcode = models.CharField(max_length=10, unique=True)
+    postcode = models.CharField(max_length=10, unique=True, db_index=True)
     area = models.CharField(max_length=2)
     date_introduced = models.CharField(blank=True, null=True)
     date_terminated = models.CharField(blank=True, null=True)
