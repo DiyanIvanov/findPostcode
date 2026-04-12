11035361b0afc2359bf078381ccad162ccdab7a2
Diyan Ivanov <coldys_@hotmail.com>
2026-04-12 10:21:47 +0100

    feat: set Swagger UI as default landing page

--- findPostcode/urls.py
+++ findPostcode/urls.py
@@ -20,9 +20,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("rest_framework.urls")),
     path("", include("accounts.urls")),
     path("api/", include("api.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
